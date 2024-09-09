8840c8e75b9b4e179a50ec9520d147ee34db9bbb
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2024-09-09 08:02:20 +0100

    Installed dependencies, added favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
+  darkMode: ["class"],
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
   },
-  plugins: [],
-}
-
+  plugins: [tailwindcssAnimate],
+};
