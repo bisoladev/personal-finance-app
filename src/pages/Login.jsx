6e5c318e9e8bc4890f9d8a9bf3e0cc37e724234d
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2024-09-12 12:18:11 +0100

    Created login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ const Login = () => {
   }
 
   return (
-    <div className="bg-beige100 flex items-center h-screen md:pl-5 p-4 md:p-0">
+    <div className="bg-beige100 flex lg:items-center h-screen lg:pl-5">
       <div
         className={cn(
           "bg-cover bg-auth w-[500px] h-[calc(100vh-40px)] rounded-[12px] hidden lg:flex flex-col justify-between p-7"
@@ -44,59 +44,67 @@ const Login = () => {
           </p>
         </div>
       </div>
-      <div className="bg-white mx-auto max-w-[560px] min-h-[422px] w-full p-8 rounded-xl">
-        <h4 className="text-preset-1 text-grey900 mb-8">Login</h4>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="space-y-4 text-grey500"
-          >
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
+      <div className="w-full">
+        <div className="bg-grey900 w-full p-6 lg:hidden">
+          <img src={Logo} alt="Finance" className="w-fit mx-auto" />
+        </div>
+        <div className="flex h-[calc(100vh-80px)] items-center p-4">
+          <div className="bg-white mx-auto max-w-[560px] min-h-[422px] w-full p-8 rounded-xl">
+            <h4 className="text-preset-1 text-grey900 mb-8">Login</h4>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4 text-grey500"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="!my-8">
-              <Button className="w-full" type="submit">
-                Login
-              </Button>
-            </div>
-            <p className="text-center">
-              Need to create an account?{" "}
-              <span>
-                <Link
-                  to="/register"
-                  className="underline text-grey900 text-preset-4-bold"
-                >
-                  Sign up
-                </Link>
-              </span>
-            </p>
-          </form>
-        </Form>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="!my-8">
+                  <Button className="w-full" type="submit">
+                    Login
+                  </Button>
+                </div>
+                <p className="text-center">
+                  Need to create an account?{" "}
+                  <span>
+                    <Link
+                      to="/register"
+                      className="underline text-grey900 text-preset-4-bold"
+                    >
+                      Sign up
+                    </Link>
+                  </span>
+                </p>
+              </form>
+            </Form>
+          </div>
+        </div>
       </div>
     </div>
   );
